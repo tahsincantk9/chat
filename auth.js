c98a1209bc380c8cac65b607d0e265ce8d0420b0
tahsincantk9 <tahsincantk9@gmail.com>
2026-06-01 20:14:26 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+window.login = function () {
+  // başarılı login sonrası:
+  window.location.href = "room.html";
+};
+
+window.register = function () {
+  // başarılı kayıt sonrası:
+  window.location.href = "room.html";
+};
